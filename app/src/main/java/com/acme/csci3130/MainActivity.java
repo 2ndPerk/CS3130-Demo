2361440e5a58746714790075cc17570297a80887
2ndPerk <tymonek.wl@gmail.com>
2017-05-11 01:57:18 -0300

    added button and text edit UI and functionality

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -1,7 +1,10 @@
 package com.acme.csci3130;
 
 import android.app.Activity;
+import android.icu.text.MessageFormat;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnCLick(View v){
+        TextView a = (TextView)findViewById(R.id.helloText);
+        TextView b = (TextView)findViewById(R.id.editText);
+        a.setText(b.getText());
     }
 }
